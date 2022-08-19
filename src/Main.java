318f4f8000de6d301d3afcba69652f864faa58bb
catalina-2022 <estudioyury@gmail.com>
2022-08-19 12:02:23 -0500

    creando presentacion del integrante 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        // Crear un objeto de la clase Integrante y definimos sus atributos segun el rol dentro del equipo
+        Integrante integrante4 = new Desarrollador("Yury Catalina Taborda Passos" , "21-22");
+        integrante4.presentacion();
+    // Crear un objeto de la clase Integrante y definimos sus atributos segun el rol dentro del equipo
         Integrante integrante5 = new ScrumMaster("Jeyson David Zuñiga Gomez","21-22");
         // Llamamos al método presentacion para mostrar la presentación del integrante
         integrante5.presentacion();
