04a4263f7f0c4f6637b6e10c2a2d9c875261f9c4
malejis140292 <malejis140292>
2022-08-19 18:34:21 -0500

    Creacion integrante alejandra

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Integrante integrante4 = new Desarrollador("Yury Catalina Taborda Passos" , "21-22");
+        Integrante integrante3 = new Desarrollador("Mayra Alejandra Moreano Vásquez" , "7-8");
         integrante4.presentacion();
-    // Crear un objeto de la clase Integrante y definimos sus atributos segun el rol dentro del equipo
+        integrante3.presentacion();
+        // Crear un objeto de la clase Integrante y definimos sus atributos segun el rol dentro del equipo
         Integrante integrante5 = new ScrumMaster("Jeyson David Zuñiga Gomez","21-22");
         // Llamamos al método presentacion para mostrar la presentación del integrante
         integrante5.presentacion();
