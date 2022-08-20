c6c7109e077fad1d94ee5265797173e8191a8dc4
JeysonZG <jdzg1124@gmail.com>
2022-08-20 07:23:06 -0500

    Organizacion de la clase Main

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,14 @@ public class Main {
         Integrante integrante2 = new Desarrollador("Carlos Mario Mora Delgado","21-22");
         Integrante integrante3 = new Desarrollador("Mayra Alejandra Moreano Vásquez" , "7-8");
         Integrante integrante4 = new Desarrollador("Yury Catalina Taborda Passos" , "21-22");
+        Integrante integrante5 = new ScrumMaster("Jeyson David Zuñiga Gomez","21-22");
 
         integrante1.presentacion();
         integrante2.presentacion();
         integrante3.presentacion();
         integrante4.presentacion();
-
-
-        // Crear un objeto de la clase Integrante y definimos sus atributos segun el rol dentro del equipo
-        Integrante integrante5 = new ScrumMaster("Jeyson David Zuñiga Gomez","21-22");
-        // Llamamos al método presentacion para mostrar la presentación del integrante
         integrante5.presentacion();
-        // Llamamos al método mensajeEquipo para mostrar el mensaje del equipo
-        integrante5.mensajeEquipo();
 
+        Integrante.mensajeEquipo();
     }
 }
